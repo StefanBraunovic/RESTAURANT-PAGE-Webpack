da05e74f037540a0217bf6c386c1f8f86c002875
Stefan Braunovic <stefan.braunovic55@gmail.com>
2020-12-03 15:36:12 +0100

    contact form added

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -8,16 +8,45 @@ function setBtnActive(id){
 }
 
 
+function createContact(){
+    const contact  = document.createElement('div');
+    contact.classList.add('contact');
+    
+    const phone= document.createElement('p');
+    phone.innerHTML="+382-67-668-988";
+    phone.classList.add('phone');
 
+    const email= document.createElement('p');
+    email.innerHTML="stefan.braunovic55@gmail.com";
+    email.classList.add('email');
+
+    const gitlink= document.createElement('a');
+    gitlink.innerHTML="github"
+
+    gitlink.href="https://github.com/StefanBraunovic";
+    gitlink.classList.add('git');
+
+    
+
+    contact.appendChild(phone);
+    contact.appendChild(email);
+    contact.appendChild(gitlink);
+
+    return contact;
+}
 
 
 
 function loadContact(){
     const content = document.getElementById('tab-content');
-    
-    content.textContent="Test";
+    content.classList.add('grid-lay');
+    content.classList.remove('flex-lay');
+    content.textContent="";
 
    setBtnActive('contact');
+   const contact = createContact();
+
+   content.appendChild(contact)
     
 }
 
--- src/modules/home.js
+++ src/modules/home.js
@@ -19,10 +19,12 @@ function createAboutSection(){
 function setBtnActive(id){
     const activeBtn = document.querySelector('.tab.active');
     if(activeBtn) activeBtn.classList.remove('active');
+    
 
     const homeBtn= document.getElementById(id);
     homeBtn.classList.add('active');
 }
+
 
 
 function loadHome(){
